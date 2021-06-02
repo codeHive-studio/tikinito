dad2e8f57da5b3e24570ffab7ee577c485ba8a84
manv6 <evlastos@gmail.com>
2021-06-02 12:52:08 +0300

    adding port

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const { createPool } = require("mysql");
 
 var pool = createPool({
-  host: "localhost:3306",
+  host: "localhost",
+  port: "3306",
   user: "j126922gil_tikinito",
   password: "T%k60e2e",
   database: "j126922gil_tikinito",
