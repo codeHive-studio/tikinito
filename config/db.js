96dd1f1d57f3ab0219c8f99cfc40a06d706783f3
manv6 <evlastos@gmail.com>
2021-06-02 12:43:51 +0300

    replace 127.0.0.1 with localhost

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { createPool } = require("mysql");
 
 var pool = createPool({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "j126922gil_tikinito",
   password: "T%k60e2e",
   database: "j126922gil_tikinito",
