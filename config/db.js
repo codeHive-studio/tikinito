9b32d4ddc5addaca8cedb4408447998be41c005c
manv6 <evlastos@gmail.com>
2021-06-02 12:39:19 +0300

    adding plesk db configuration

--- config/db.js
+++ config/db.js
@@ -1,20 +1,11 @@
 const { createPool } = require("mysql");
 
-// var pool = createPool({
-//   host: "103.195.4.8",
-//   user: "devicefilter",
-//   password: "esh@len$1",
-//   database: "devicefilter_db",
-//   connectionLimit: 200000,
-//   multipleStatements: true,
-// });
-
 var pool = createPool({
   host: "127.0.0.1",
-  user: "root",
-  password: "manolo123!@#",
-  database: "devicefilter_db",
-  connectionLimit: 200,
+  user: "j126922gil_tikinito",
+  password: "T%k60e2e",
+  database: "j126922gil_tikinito",
+  connectionLimit: 2000,
   multipleStatements: true,
 });
 
